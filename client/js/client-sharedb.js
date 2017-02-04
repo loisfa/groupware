158767ca7928ae70685dfd993869b65b86753e73
loisfa <lois94@hotmail.fr>
2017-02-04 15:47:29 +0100

    first commit in add_sharedb branch. no error anymore from servor when receiving non-string message.

---
+++ client/js/client-sharedb.js
@@ -0,0 +1,148 @@
+
+var ShareDB = require('sharedb/lib/client');
+var StringBinding = require('sharedb-string-binding');
+
+// Open WebSocket connection to ShareDB server
+var socket = new WebSocket('ws://' + window.location.host);
+var connection = new ShareDB.Connection(socket);
+/*socket.send(JSON.stringify({
+  id: "client1"
+}));*/
+
+
+
+/*
+var pathname = window.location.pathname;
+console.log("pathname1:" + pathname);
+pathname = pathname.split("/")[2];
+pathname = pathname.replace(/\//g, '');
+console.log("pathname2:" + pathname);
+*/
+
+/*
+var myWidgets = [];
+var myFormerWidgets = JSON.parse(localStorage.getItem('myWidgets'));
+myFormerWidgets === null ? console.log('No prior widget found') : myWidgets = myFormerWidgets;
+*/
+
+// Create local Doc instance mapped to 'examples' collection document with id 'textarea'
+socket.onmessage(function() {
+  console.log("message arrivé");
+}
+
+socket.send("data");
+
+var doc = connection.get('examples', 'documents');
+console.log("scoket.data: "+ socket.data);
+
+doc.subscribe(function(err) {
+    console.log("subscribe");
+    if (err) throw err;
+    if (doc.type === null) {
+        doc.create('new data');
+        console.log("doc.type === null");
+    }
+    console.log("doc.type != null");
+
+    var element = document.getElementById('content-list');
+    var binding = new StringBinding(element, doc);
+    binding.setup();
+    documentChange();
+    doc.on('op',documentChange);
+});
+
+var documentChange = function(){
+    doc.data = document.getElementById('content-list').innerHTML;
+    console.log("documentChange: doc.data: "+doc.data);
+    socket.send(JSON.stringify({
+      id: "client1"
+    }));
+}
+
+document.getElementById('content-list').oninput = documentChange ;
+
+
+/*
+// Create local Chat instance
+var chat = connection.get('chat', 'chat-msg');
+doc.subscribe(function(err){
+    if (err) throw err;
+              var chatlist = document.getElementById('chat');
+    var binding = new StringBinding(chatlist, chat);
+    binding.setup();
+    chatChange();
+    chat.on('op', chatChange);
+});
+*/
+
+
+/*
+var chatChange = function(){
+    var chatMsg = document.getElementById('chat').innerText;
+}
+*/
+
+// document.getElementById('chat').oninput = chatChange;
+/*
+window.onload = function(){
+
+    var em_script = document.createElement('script');
+    em_script.src = '/static/em_button.js';
+
+    document.getElementById('editable-editor').insertBefore(em_script, document.getElementById('editable-editor').childNodes[0]);
+}
+*/
+// Ui add modal stuff
+
+
+/*
+var modal = document.getElementById('ui-add-modal');
+var btn = document.getElementById("ui-add-button");
+var closeButton = document.getElementById("close-button");
+var widgetUrlButton = document.getElementById('widget-url-button');
+*/
+
+/*
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+var closeModale = function() {
+    modal.style.display = "none";
+}
+
+closeButton.onclick = closeModale;
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+*/
+
+/*
+widgetUrlButton.onclick = function() {
+    var widgetUrl = document.getElementById('widget-url-input').value;
+    addWidget(widgetUrl);
+
+    myWidgets.push(widgetUrl);
+    localStorage.setItem('myWidgets',JSON.stringify(myWidgets));
+
+    document.getElementById('widget-url-input').value = "";
+    closeModale();
+}
+*/
+
+/*
+var addWidget = function(widgetUrl) {
+
+    var em_script = document.createElement('script');
+    em_script.src = widgetUrl;
+    document.getElementById('editable-editor').insertBefore(em_script, document.getElementById('editable-editor').childNodes[0]);
+}
+
+
+myWidgets.forEach(function(widget){
+    addWidget(widget);
+})
+*/
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+
+
 var meetupsController = require('./controllers/meetups-controller.js');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
@@ -8,16 +10,60 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/mean-demo');
 
 app.use(bodyParser());
-
 app.get('/', function(req,res) {
   res.sendFile(path.resolve(__dirname + '/../client/index.html'));
 });
-
 app.post('/api/meetups', meetupsController.create);
 app.get('/api/meetups', meetupsController.list);
-
 app.use('/js', express.static(path.resolve(__dirname + '/../client/js')));
 
+
 app.listen(3000, function() {
-  console.log('listening...');
+  console.log('Listening on port 3000...');
 });
+
+//app.use('/static',express.static('../client'));
+/*
+app.get(/edit\/..+/,function(req, res){
+    res.sendFile('editor.html',{root:'../client'});
+});
+app.get(/read\/..+/,function(req, res){
+    res.sendFile('reader.html',{root:'../client'});
+});
+app.get('/', function(req, res){
+    res.redirect('/static');
+});
+*/
+
+// sharedb
+var http = require('http');
+var ShareDB = require('sharedb');
+var WebSocket = require('ws');
+var WebSocketJSONStream = require('websocket-json-stream');
+
+// Maybe the mongo creation should also be in the startServer function?
+const db = require('sharedb-mongo')('mongodb://localhost:27017/test');
+const backend = new ShareDB({db});
+
+var server = http.createServer(app);
+
+// Connect any incoming WebSocket connection to ShareDB
+var wss = new WebSocket.Server({server: server});
+
+wss.on('connection', function(ws, req) {
+    console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa");
+    var stream = new WebSocketJSONStream(ws);
+    console.log("BBBBBBBBBBBBB");
+    backend.listen(stream);
+    console.log("ws: "+ws+" stream: "+stream.data);
+});
+
+
+wss.on('message', function incoming(data, flags) {
+  // flags.binary will be set if a binary data is received.
+  // flags.masked will be set if the data was masked.
+  console.log("received message");
+});
+
+server.listen(8080);
+console.log('Listening on http://localhost:8080');
